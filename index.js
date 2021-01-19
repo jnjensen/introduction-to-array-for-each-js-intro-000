128fca08180969c1dc5e580126e756c4da72768b
James Jensen <jnjensen@live.com>
2021-01-19 03:37:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Add your doToElementsInArray() function here:
-
+function doToElementsInArray() {
+}
 
 // Add your changeCompletely() function here:
+function changeCompletely(word){
+ word.forEach(ele => {ele+"ed"})
+}
